f17ad23d3936f483ca5e776de34de432e56ec835
arcangeldoc <arcangeldoc@13ff6194-6f6a-4844-bc04-381cf2dcba09>
2011-12-06 18:28:53 +0000

    * class_conexion.cs:
    git-svn-id: http://svn.code.sf.net/p/systemsosirisho/code@103 13ff6194-6f6a-4844-bc04-381cf2dcba09

--- osiris/class_conexion.cs
+++ osiris/class_conexion.cs
@@ -10,7 +10,7 @@ namespace osiris
 		public string _url_servidor = "Server=192.168.1.10;";
         public string _port_DB = "Port=5432;";
         public string _usuario_DB = "User ID=admin;";
-        public string _passwrd_user_DB = "Password=1qaz2wsx;";
+        public string _passwrd_user_DB = "Password=1qaz2wsx123;";
 		public string _nombrebd = "Database=osiris_produccion";
 	}
 }
